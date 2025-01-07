5d1d240c18399ff6149eaf97c520a90f346531d2
kylew2001 <kswalker2201@gmail.com>
2025-01-07 18:05:50 +1300

    commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Type casting = convert the data type of a value to another data type.
+x = 1 #int
+y = 2.0 #float
+z = "3" #String
+
+# x = float(x) #Convert to different data type
+# y = float(y)
+# z = float(z)
+
+
+print("X is "+str(x))
+print("Y is "+str(y))
+print(z*3)
